feea5bc16da20f8eb42109b402b150592829a98e
Alejandro Ortiz <ortizalejandro93@gmail.com>
2022-02-12 19:46:59 -0800

    Q3a 1/1

--- Programming Assignments/PS3/reinforcement/analysis.py
+++ Programming Assignments/PS3/reinforcement/analysis.py
@@ -25,9 +25,9 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = .9
+    answerNoise = .2
+    answerLivingReward = -2.5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
